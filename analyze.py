4284dc3144faee36c32a57f052a09b5503a0abd3
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-10-24 21:34:21 +0200

    drop slabinfo

--- analyze.py
+++ analyze.py
@@ -2,8 +2,58 @@
 import argparse
 import glob
 import os
+import re
 
-from slabinfo import slabinfo
+
+class slabinfo:
+
+    def __init__(self, entries, version):
+        self.entries = entries
+        self.version = version
+
+    @property
+    def total_size(self):
+        return sum(entry.total_size for entry in self.entries)
+
+    @classmethod
+    def from_stream(cls, stream):
+        header = stream.readline()
+        m = re.match("^slabinfo - version: (\d+\.\d+)$", header)
+        if m is None:
+            raise ValueError("cannot parse slabinfo header: {!a}".format(
+                header))
+        version = m.group(1)
+        hint = stream.readline()
+        if not hint.startswith("#"):
+            raise ValueError("cannot parse slabinfo hint: {!a}".format(hint))
+        entries = []
+        for line in stream.readlines():
+            entry = slabinfo_entry.from_string(line)
+            entries.append(entry)
+        return cls(entries, version)
+
+
+class slabinfo_entry:
+
+    def __init__(self, name, active_objs, num_objs, objsize):
+        self.name = name
+        self.active_objs = active_objs
+        self.num_objs = num_objs
+        self.objsize = objsize
+
+    @property
+    def total_size(self):
+        # NOTE: ignoring active_objs and using num_objs a this feels more
+        # representative of the actual memory requirements.
+        return self.num_objs * self.objsize
+
+    @classmethod
+    def from_string(cls, string):
+        m = re.match("^([a-zA-Z0-9_-]+)\s+(\d+)\s+(\d+)\s+(\d+)", string)
+        if m is None:
+            raise ValueError("cannot parse slabinfo entry: {!a}", string)
+        return cls(m.group(1), int(m.group(2)), int(m.group(3)),
+                   int(m.group(4)))
 
 def collect_traces(dirname, name):
     pattern = os.path.join(dirname, "slabinfo.{}.*".format(name))
