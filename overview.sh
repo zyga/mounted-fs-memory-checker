5358c3ffb17c70e2e70999325724f1f48723a1d2
Zygmunt Krynicki <me@zygoon.pl>
2016-10-25 18:55:07 +0200

    Make overview script a bit nicer

--- overview.sh
+++ overview.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
-./analyze.py fedora 24 4.7.9-200.fc24.x86_64 1 size-1m.squashfs.xz.heavy
-./analyze.py ubuntu 16.04 4.4.0-45-generic 1 size-1m.squashfs.xz.heavy
-./analyze.py ubuntu 16.04 4.4.0-45-generic 4 size-1m.squashfs.xz.heavy
-./analyze.py ubuntu 16.10 4.8.0-26-generic 1 size-1m.squashfs.xz.heavy
+variant=size-1m.squashfs.xz.heavy
+./analyze.py fedora 24 4.7.9-200.fc24.x86_64 1 $variant
+./analyze.py ubuntu 16.04 4.4.0-45-generic 1 $variant
+./analyze.py ubuntu 16.04 4.4.0-45-generic 4 $variant
+./analyze.py ubuntu 16.10 4.8.0-26-generic 1 $variant
