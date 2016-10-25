c860e5aa1b88e7f0dd69cd7b11db40431f3d38e5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2016-10-25 18:49:30 +0200

    Add 4-core 16.04 trace to overview

--- overview.sh
+++ overview.sh
@@ -2,4 +2,5 @@
 set -x
 ./analyze.py fedora 24 4.7.9-200.fc24.x86_64 1 size-1m.squashfs.xz.heavy
 ./analyze.py ubuntu 16.04 4.4.0-45-generic 1 size-1m.squashfs.xz.heavy
+./analyze.py ubuntu 16.04 4.4.0-45-generic 4 size-1m.squashfs.xz.heavy
 ./analyze.py ubuntu 16.10 4.8.0-26-generic 1 size-1m.squashfs.xz.heavy
